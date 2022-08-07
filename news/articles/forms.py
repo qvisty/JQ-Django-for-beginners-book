e56048961480bafede95a2d5fa1da5397e4b0746
Qvisty <jgq@live.dk>
2022-08-07 17:13:27 +0200

    Update forms.py

--- news/articles/forms.py
+++ news/articles/forms.py
@@ -5,6 +5,7 @@ from .models import Comment
 
 # https://docs.djangoproject.com/en/4.0/topics/forms/modelforms/#modelform
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
